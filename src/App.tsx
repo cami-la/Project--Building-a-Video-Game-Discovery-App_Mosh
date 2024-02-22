db28b593daabbfd8351ae0683ae2d97279fc4f4b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-22 11:21:36 -0300

    :lipstick: Build a responsive layout
    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="teal">Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="tomato">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="papayawhip">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="lightgreen">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
