59f4b2f1c9708e5bad4f0be52efb7b0c63dacbda
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-22 11:45:07 -0300

    :construction: Build the color mode switch
    build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="papayawhip">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="lightgreen">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,15 @@
+import { HStack, Switch, Text, color, useColorMode } from "@chakra-ui/react";
+import React from "react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch colorScheme="green" isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
