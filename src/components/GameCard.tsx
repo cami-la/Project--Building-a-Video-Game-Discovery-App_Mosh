55189c519ad34e03ad5e676045913f59aa0914f1
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-23 13:39:41 -0300

    :sparkles: Building Game Cards
    building Game Cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <div>
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={game.background_image} alt={game.name} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,18 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const {games, error} = useGames();
+  const { games, error } = useGames();
 
   return (
     <>
-      <ul>
-        {error && <Text>{error}</Text>}
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={10}>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
