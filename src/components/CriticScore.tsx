d74422d582274e4456acf6f3652f620669a95608
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-29 09:35:04 -0300

    :sparkles: Displaying critic score
    displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color: string = score > 75 ? "green" : score > 50 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlataformIconList from "./PlataformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,7 +14,12 @@ const GameCard = ({ game }: Props) => {
         <Image src={game.background_image} alt={game.name} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
-          <PlataformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+          <HStack justifyContent={"space-between"}>
+            <PlataformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
         </CardBody>
       </Card>
     </div>
