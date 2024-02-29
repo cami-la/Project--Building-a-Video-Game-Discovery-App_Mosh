697844f66807494ad7ab71760d2c73d59e90cfa7
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-29 11:39:57 -0300

    :sparkles: Show Loading Skeletons
    Show Loading Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,8 +11,11 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <div>
-      <Card borderRadius={10} overflow="hidden">
-        <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
+      <Card width={"300px"} borderRadius={10} overflow="hidden">
+        <Image
+          src={getCroppedImageUrl(game.background_image)}
+          alt={game.name}
+        />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           <HStack justifyContent={"space-between"}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,22 +23,29 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
