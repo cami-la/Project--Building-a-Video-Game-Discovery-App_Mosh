0e63a8b673cfa057513d14b918ad17f47d33702f
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-02-29 12:04:56 -0300

    :sparkles: Fetch the genres
    fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = useGenres();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    apiClient
+      .get<FetchGenresResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
