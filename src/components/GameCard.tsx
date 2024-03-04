6af969e77e0e12174993164537dbd311ef27d19e
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-03-04 16:52:32 -0300

    :sparkles: Display genres
    display genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <div>
-      <Card width={"300px"} borderRadius={10} overflow="hidden">
+      <Card borderRadius={10} overflow="hidden">
         <Image
           src={getCroppedImageUrl(game.background_image)}
           alt={game.name}
