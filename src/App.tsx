352769d2e6d58f5255b1bfdbf5225b260e58e572
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-03-05 13:43:45 -0300

    :sparkles: Build and platform selector
    Build and platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlataformSelector from "./components/PlataformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -24,10 +25,14 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlataformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
---
+++ src/components/PlataformSelector.tsx
@@ -0,0 +1,23 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+
+const PlataformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+        Plataforms
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlataformSelector;
