e2bb5e04ca99582affeaad5eb7e3f6de232f8aab
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-03-05 16:22:22 -0300

    :recycle: Extract a query object
    extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -15,13 +16,12 @@ export interface Game {
 }
 
 const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatfom: Platform | null
+  gameQuery: GameQuery
 ) =>
   useData<Game>(
     "/games",
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatfom?.id } },
-    [selectedGenre?.id, selectedPlatfom?.id]
+    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+    [gameQuery]
   );
 
 export default useGames;
