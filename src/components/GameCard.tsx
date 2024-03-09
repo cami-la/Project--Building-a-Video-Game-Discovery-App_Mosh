b7f999bff225abeb87641fb5d780f4b83d381eff
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2024-03-09 15:15:14 -0300

    :sparkles: Clean up the game cards
    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -17,13 +17,13 @@ const GameCard = ({ game }: Props) => {
           alt={game.name}
         />
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlataformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </div>
